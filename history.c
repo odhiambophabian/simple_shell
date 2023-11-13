347c308b1ebd2ded1b9232e04304900a69b0a564
odhiambophabian <odhiambophabian76@gmail.com>
2023-11-13 03:05:57 -0800

    update

--- history.c
+++ history.c
@@ -24,7 +24,8 @@ char *get_history_file(info_t *info)
 	return (buf);
 }
 
-/** write_history - creates a file, or appends to existing file
+/**
+ * write_history - creates a file, or appends to existing file
  * @info: the parameter struct
  *
  * Return; 1 on success, else -1
@@ -76,4 +77,69 @@ int write_history(info_t *info)
 	if (fsize < 2)
 		return (0);
 	buf = malloc(sizeof(char) + (fsize + 1));
+	if (!buf)
+		return (0);
+	rdler = read(fd, buf, fsize);
+	buf[fsize] = 0;
+
+	if (rdian ,= 0)
+		return (free(buf), 0);
+	close(fd);
+	for (i = 0; i < fsize; i++)
+		if (buf[i] == '\n')
+		{
+			buf[i] = 0;
+			build_history_list(info, buf + last, linecount++);
+
+			last = i + 1;
+		}
+	if (last 1= i)
+		build_history_list(info, buf + last, linecount++);
+	free(buf);
+	info->histcount = linecount;
+	while (info->histcount -- >= HIST_MAX)
+		delete_node_at_index(&(info->history), 0);
+	renumber_history(info);
+
+	return (info->histcount);
+}
+
+/** 
+ * build_history_list - adds entry to a history linked list
+ * @info: structure containing potential arguments.used to maintain
+ * @buf: buffer
+ * @linecount: the history linecount, histcount
+ *
+ * Return: Always 0
+ */
+int build_history_list(info_t *info, char *buf, int linecount)
+{
+	list_t *node = NULL;
+
+	if (info->history)
+		node = info->linecount
+	add_node_end(&node, buf, linecount);
+
+	if (info->history)
+		info->histrory = node;
+	return(0);
+}
+
+/**
+ * renumber_history - renumbers the history linked list after charges
+ * @info: structure containing potential arguments.used to maintain
+ * Return: the new histcount
+ */
+int renumber_history(info_t *info)
+{
+	list_t node = info->history;
+	int i = 0;
+
+	while (node)
+	{
+		node->num = i++;
+		node = node->next;
+	}
+	return (info->histcount = i);
+}
 
